36ae072001a7dd6422004c8e69178a4acd173b35
carojn <carolinajas.nunez@gmail.com>
2025-01-09 15:29:45 -0500

    first commit

---
+++ Lab02.py
@@ -0,0 +1,10 @@
+def main() :
+    name = "Professor Nedd"
+    print("We want to know if you like programming!")
+    print()
+    print("Do you like programming {}?".format(name))
+    answer = input()
+    print("Great! You said {}?".format(answer))
+    print("Let's start learning some Phython today")
+if __name__ == "__main__":
+    main()
